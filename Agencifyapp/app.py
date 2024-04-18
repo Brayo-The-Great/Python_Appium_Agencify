81d1429f8ff6659338f197a51d884002af1a3f87
Brian Waititu Muraya <brianwaititumuraya@Brians-MacBook-Air.local>
2024-04-18 16:46:36 +0300

    merging pull request

--- Agencifyapp/app.py
+++ Agencifyapp/app.py
@@ -3,7 +3,7 @@ from time import sleep
 from typing import Any, Dict
 from datetime import datetime
 from appium import webdriver
-from appium.webdriver.common.touch_action import TouchAction
+# from appium.webdriver.common.touch_action import TouchAction
 import time
 from appium.options.android import UiAutomator2Options
 from appium.options.common import AppiumOptions
@@ -16,12 +16,12 @@ from selenium.common.exceptions import ElementNotVisibleException, NoSuchElement
 capabilities = dict(
     platformName='Android',
     automationName='uiautomator2',
-    platformVersion='13.0',
-    deviceName='OPPO CPH2473',
+    platformVersion='14.0',
+    deviceName='Android',
     language='en',
     locale='US',
     autoGrantPermissions='true',
-    app=r'C:\Users\PC\Desktop\TESTAPKs\staging20348.apk'
+    app=r'/Users/brianwaititumuraya/Documents/TESTAPKs/20348.apk'
 
 )
 
@@ -87,10 +87,11 @@ class TestAppium(unittest.TestCase):
                                       value='//android.widget.ScrollView/android.view.View[''1]/android.widget'
                                             '.EditText/android.widget.Button')
         el.click()
-        self.driver.implicitly_wait(60)
+        self.driver.implicitly_wait(90)
 
         current_date = datetime.now().strftime('%a, %d %b')  # Format like 'Wed, 28 Feb'
         xpath = f'//android.widget.TextView[@content-desc="{current_date}"]'
+
         el = self.driver.find_element(by=AppiumBy.XPATH, value=xpath)
         el.click()
 
@@ -149,99 +150,11 @@ class TestAppium(unittest.TestCase):
             (By.XPATH, '//android.widget.Button[@resource-id="insure.agencify.agencify:id/buy"]')))
         el.click()
 
-        el = WebDriverWait(self.driver, 90).until(EC.element_to_be_clickable(
-            (By.XPATH,
-             '//android.widget.Button[@resource-id="insure.agencify.agencify:id/btn_close_dialog_amend_cover_date"]')))  # Got it dialogue pop up
-        el.click()
+        # el = WebDriverWait(self.driver, 90).until(EC.element_to_be_clickable(By.XPATH, '//*[@text="Select a client"]'))
 
-        el = WebDriverWait(self.driver, 90).until(EC.element_to_be_clickable(
-            (By.XPATH,
-             '//android.widget.Button[@resource-id="insure.agencify.agencify:id/addClien"]')))  # Select a client button
-        el.click()
-
-        el = WebDriverWait(self.driver, 90).until(EC.element_to_be_clickable(
-            (By.XPATH,
-             '//android.widget.Button[@resource-id="insure.agencify.agencify:id/addClient"]')))  # New client button
-        el.click()
-
-        el = WebDriverWait(self.driver, 90).until(EC.element_to_be_clickable(
-            (By.XPATH,
-             '//android.widget.ImageButton[@content-desc="Show drop-down menu"]')))  # Client Type button
-        el.click()
-
-        el = WebDriverWait(self.driver, 90).until(EC.element_to_be_clickable(
-            (By.XPATH,
-             '//android.widget.EditText[@resource-id="insure.agencify.agencify:id/firstName"]')))  # Individual button
-        el.click()
-
-        el = WebDriverWait(self.driver, 90).until(EC.element_to_be_clickable(
-            (By.XPATH,
-             '//android.widget.EditText[@resource-id="insure.agencify.agencify:id/firstName"]'))).send_keys(
-            "QA")  # First Name
-
-        el = WebDriverWait(self.driver, 90).until(EC.element_to_be_clickable(
-            (By.XPATH,
-             '//android.widget.EditText[@resource-id="insure.agencify.agencify:id/last_name"]'))).send_keys(
-            "ONE")  # Last Name
-
-        el = WebDriverWait(self.driver, 90).until(EC.element_to_be_clickable(
-            (By.XPATH,
-             '//android.widget.EditText[@resource-id="insure.agencify.agencify:id/mobile"]'))).send_keys(
-            "0759404954")  # Mobile Number
-
-        el = WebDriverWait(self.driver, 90).until(EC.element_to_be_clickable(
-            (By.XPATH,
-             '//android.widget.EditText[@resource-id="insure.agencify.agencify:id/id_no"]'))).send_keys(
-            "64775697")  # National ID
-
-        el = WebDriverWait(self.driver, 90).until(EC.element_to_be_clickable(
-            (By.XPATH,
-             '//android.widget.EditText[@resource-id="insure.agencify.agencify:id/pin"]'))).send_keys(
-            "A865356896J")  # KRA PIN
-
-        el = WebDriverWait(self.driver, 90).until(EC.element_to_be_clickable(
-            (By.XPATH,
-             '//android.widget.EditText[@resource-id="insure.agencify.agencify:id/email"]'))).send_keys(
-            "waititubrian9@gmail.com")  # Email
-
-        el = WebDriverWait(self.driver, 90).until(EC.element_to_be_clickable(
-            (By.XPATH,
-             '//android.widget.RadioButton[@resource-id="insure.agencify.agencify:id/male"]')))  # Gender - Male
-        el.click()
-
-        el = WebDriverWait(self.driver, 90).until(EC.element_to_be_clickable(
-            (By.XPATH,
-             '//android.widget.Button[@resource-id="insure.agencify.agencify:id/save_client"]')))  # Save button
-        el.click()
-
-        # user_input = input("Please enter the code sent to your phone number: ")
-        # code = user_input  # Example code
-        # if len(user_input) != 4:
-        #     print("Error: The code should be exactly 4 characters long.")
-        #     exit()
-        #
-        # print(f"Entering OTP: {code}")
-        #
-        # # Assuming this is the correct way to fetch the elements, adjust if needed
-        # element1 = self.driver.find_element(By.XPATH,
-        #                                     '//androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View[1]/android.widget.EditText[1]/android.view.View')
-        # element2 = self.driver.find_element(By.XPATH,
-        #                                     '//androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View[1]/android.widget.EditText[2]/android.view.View')
-        # element3 = self.driver.find_element(By.XPATH,
-        #                                     '//androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View[1]/android.widget.EditText[3]/android.view.View')
-        # element4 = self.driver.find_element(By.XPATH,
-        #                                     '//androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View[1]/android.widget.EditText[4]/android.view.View')
-        #
-        # # Sending the code to each input field
-        # element1.send_keys(code[0])
-        # # Add a short delay between entering each digit, adjust if needed
-        # time.sleep(1)
-        # element2.send_keys(code[1])
-        # time.sleep(1)
-        # element3.send_keys(code[2])
-        # time.sleep(1)
-        # element4.send_keys(code[3])
-
+        # el = WebDriverWait(self.driver, 90).until(EC.element_to_be_clickable(
+        #     (By.XPATH, '//android.widget.Button[@resource-id="insure.agencify.agencify:id/addClien"]')))
+        # el.click()
 
 
 if __name__ == '__main__':
